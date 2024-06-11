9a90744f07a9ebedcf8bd162e40e5391eab2bd37
Erick  Serejo <erickrainier@hotmail.com>
2024-06-11 19:36:24 -0300

    communication

--- Weather App/Weather App/DailyForecastTableViewCell.swift
+++ Weather App/Weather App/DailyForecastTableViewCell.swift
@@ -4,17 +4,15 @@
 //
 //  Created by Erick Serejo on 11/06/24.
 //
-
 import UIKit
 
 class DailyForecastTableViewCell: UITableViewCell {
     
-    static let indentifier: String = "DailyForecast"
+    static let identifier: String = "DailyForecast"
     
     private lazy var weekDayLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "TER"
         label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         return label
@@ -23,7 +21,6 @@ class DailyForecastTableViewCell: UITableViewCell {
     private lazy var minTemperatureLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "min 25'C"
         label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         return label
@@ -32,7 +29,6 @@ class DailyForecastTableViewCell: UITableViewCell {
     private lazy var maxTemperatureLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "max 25'C"
         label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         return label
@@ -42,21 +38,21 @@ class DailyForecastTableViewCell: UITableViewCell {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "cloudIcon")
         return imageView
     }()
     
     private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(
-        arrangedSubviews: [
-        weekDayLabel, iconImageView, minTemperatureLabel, maxTemperatureLabel])
+        let stackView = UIStackView(arrangedSubviews: [weekDayLabel,
+                                                       iconImageView,
+                                                       minTemperatureLabel,
+                                                       maxTemperatureLabel])
         stackView.axis = .horizontal
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 16,
-                                                                          leading: 16,
-                                                                          bottom: 16,
-                                                                          trailing: 16)
+                                                                     leading: 16,
+                                                                     bottom: 16,
+                                                                     trailing: 16)
         stackView.spacing = 15
         return stackView
     }()
@@ -66,27 +62,34 @@ class DailyForecastTableViewCell: UITableViewCell {
         setupView()
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupView(){
-        backgroundColor = .clear
-        selectionStyle = .none
-        setHierarchy()
-        setConstrains()
+    func loadData(weekDay: String?, min: String?, max: String?, icon: UIImage?) {
+        weekDayLabel.text = weekDay
+        minTemperatureLabel.text = "min \(min ?? "")"
+        maxTemperatureLabel.text = "max \(max ?? "")"
+        iconImageView.image = icon
     }
     
-    private func setHierarchy(){
+    private func setupView() {
+        backgroundColor = .clear
+        selectionStyle = .none
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
         contentView.addSubview(stackView)
+        
         NSLayoutConstraint.activate([
             weekDayLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 50)
         ])
     }
     
-    private func setConstrains(){
+    private func setConstraints() {
         stackView.setConstraintsToParent(contentView)
     }
-    
 }
--- Weather App/Weather App/HourlyForecastCollectionViewCell.swift
+++ Weather App/Weather App/HourlyForecastCollectionViewCell.swift
@@ -7,22 +7,23 @@
 
 import UIKit
 
+
 class HourlyForecastCollectionViewCell: UICollectionViewCell {
     
     static let indentifier: String = "HourlyForecast"
     
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [hourLabel,
-                                                    iconImageView,
-                                                    temperatureLabel])
+                                                       iconImageView,
+                                                       temperatureLabel])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 4
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 8,
-                                                                          leading: 8,
-                                                                          bottom: 8,
-                                                                          trailing: 8)
+                                                                     leading: 8,
+                                                                     bottom: 8,
+                                                                     trailing: 8)
         stackView.layer.borderWidth = 1
         stackView.layer.borderColor = UIColor.contrastColor?.cgColor
         stackView.layer.cornerRadius = 20
@@ -32,7 +33,6 @@ class HourlyForecastCollectionViewCell: UICollectionViewCell {
     private lazy var hourLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "13:00"
         label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
         label.textAlignment = .center
@@ -42,7 +42,6 @@ class HourlyForecastCollectionViewCell: UICollectionViewCell {
     private lazy var temperatureLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "25 C"
         label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
         label.textAlignment = .center
@@ -52,7 +51,6 @@ class HourlyForecastCollectionViewCell: UICollectionViewCell {
     private lazy var iconImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = UIImage(named: "sunIcon")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
@@ -62,27 +60,30 @@ class HourlyForecastCollectionViewCell: UICollectionViewCell {
         setupView()
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupView(){
-        setHierarchy()
-        setConstrains()
-        
+    func loadData(time: String?, icon: UIImage?, temp: String?) {
+        hourLabel.text = time
+        iconImageView.image = icon
+        temperatureLabel.text = temp
     }
     
-    private func setHierarchy(){
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
         contentView.addSubview(stackView)
     }
     
-    private func setConstrains(){
+    private func setConstraints() {
         stackView.setConstraintsToParent(contentView)
+        
         NSLayoutConstraint.activate([
             iconImageView.heightAnchor.constraint(equalToConstant: 33)
         ])
-        
     }
-    
 }
